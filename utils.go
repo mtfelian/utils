053d4d3b4404f75ae51f93de85acda733f4d04cd
Artemij <atmajotii@mihteh.com>
2016-04-06 10:53:29 +0300

    Добавил пару функций

--- utils.go
+++ utils.go
@@ -5,8 +5,8 @@ import (
 	"encoding/base64"
 	"errors"
 	"math"
-	"strconv"
 	"regexp"
+	"strconv"
 )
 
 // StringToUint трактует строку s как значение типа uint
@@ -84,4 +84,123 @@ func FormatPhone(phone string) (string, error) {
 		res = "7" + res[1:len(res)]
 	}
 	return res, nil
+}
+
+var renderFloatPrecisionMultipliers = [10]float64{
+	1,
+	10,
+	100,
+	1000,
+	10000,
+	100000,
+	1000000,
+	10000000,
+	100000000,
+	1000000000,
+}
+
+var renderFloatPrecisionRounders = [10]float64{
+	0.5,
+	0.05,
+	0.005,
+	0.0005,
+	0.00005,
+	0.000005,
+	0.0000005,
+	0.00000005,
+	0.000000005,
+	0.0000000005,
+}
+
+// todo документация
+func RenderFloat(format string, n float64) (string, error) {
+	if math.IsNaN(n) {
+		return "NaN", nil
+	}
+	if n > math.MaxFloat64 {
+		return "Infinity", nil
+	}
+	if n < -math.MaxFloat64 {
+		return "-Infinity", nil
+	}
+
+	precision := 2
+	decimalStr := "."
+	thousandStr := ","
+	positiveStr := ""
+	negativeStr := "-"
+
+	if len(format) > 0 {
+		precision = 9
+		thousandStr = ""
+
+		formatDirectiveChars := []rune(format)
+		formatDirectiveIndices := make([]int, 0)
+		for i, char := range formatDirectiveChars {
+			if char != '#' && char != '0' {
+				formatDirectiveIndices = append(formatDirectiveIndices, i)
+			}
+		}
+
+		if len(formatDirectiveIndices) > 0 {
+			if formatDirectiveIndices[0] == 0 {
+				if formatDirectiveChars[formatDirectiveIndices[0]] != '+' {
+					return "", errors.New("RenderFloat(): ошибка, должен быть положительный знак")
+				}
+				positiveStr = "+"
+				formatDirectiveIndices = formatDirectiveIndices[1:]
+			}
+
+			if len(formatDirectiveIndices) == 2 {
+				if (formatDirectiveIndices[1] - formatDirectiveIndices[0]) != 4 {
+					return "", errors.New("RenderFloat(): ошибка, за разделителем разрядов тысяч должны следовать три спецификатора цифры")
+				}
+				thousandStr = string(formatDirectiveChars[formatDirectiveIndices[0]])
+				formatDirectiveIndices = formatDirectiveIndices[1:]
+			}
+
+			if len(formatDirectiveIndices) == 1 {
+				decimalStr = string(formatDirectiveChars[formatDirectiveIndices[0]])
+				precision = len(formatDirectiveChars) - formatDirectiveIndices[0] - 1
+			}
+		}
+	}
+
+	var signStr string
+	if n >= 0.000000001 {
+		signStr = positiveStr
+	} else if n <= -0.000000001 {
+		signStr = negativeStr
+		n = -n
+	} else {
+		signStr = ""
+		n = 0.0
+	}
+
+	intf, fracf := math.Modf(n + renderFloatPrecisionRounders[precision])
+
+	intStr := strconv.Itoa(int(intf))
+
+	if len(thousandStr) > 0 {
+		for i := len(intStr); i > 3; {
+			i -= 3
+			intStr = intStr[:i] + thousandStr + intStr[i:]
+		}
+	}
+
+	if precision == 0 {
+		return signStr + intStr, nil
+	}
+
+	fracStr := strconv.Itoa(int(fracf * renderFloatPrecisionMultipliers[precision]))
+	if len(fracStr) < precision {
+		fracStr = "000000000000000"[:precision-len(fracStr)] + fracStr
+	}
+
+	return signStr + intStr + decimalStr + fracStr, nil
+}
+
+// todo документация
+func RenderInteger(format string, n int64) (string, error) {
+	return RenderFloat(format, float64(n))
 }
